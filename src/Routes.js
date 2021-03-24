f028362d7306a2174a8fdea3e17be2f1f267f451
Eric Jiang <ericjiang1988@gmail.com>
2021-03-24 16:51:53 -0700

    create list all and display a note

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,9 @@ import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
+
 export default function Routes() {
     return (
         <Switch>
@@ -16,6 +19,13 @@ export default function Routes() {
             <Route exact path='/signup'>
                 <Signup />
             </Route>
+            <Route exact path='/notes/new'>
+                <NewNote />
+            </Route>
+            <Route exact path='/notes/:id'>
+                <Notes />
+            </Route>
+
             {/* Finally, catch all unmatched routes */}
             <Route>
                 <NotFound />
